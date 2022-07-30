a8ec1b9d606f2ad30b6787e130e70371e5e93166
Luis Carlos <lucaldev22@gmail.com>
2022-07-30 14:30:48 -0300

    Jest and folders configured.

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -1,10 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
   html,
 body {
-  padding: 0;
-  margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
@@ -14,7 +17,5 @@ a {
   text-decoration: none;
 }
 
-* {
-  box-sizing: border-box;
-}
+
 `;
